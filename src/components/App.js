96f5a97440ff4e935404f0b1fe6f0c8983acac7b
CHRIS_LEE <chrismlee87@gmail.com>
2020-12-02 15:21:20 -0800

    starter code

---
+++ src/components/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Title from './Title/Title';
+import POPOSList from './POPOSList/POPOSList';
+import Footer from './Footer.js';
+import { HashRouter as Router, Route } from 'react-router-dom'
+import About from './About/About'
+import { NavLink } from 'react-router-dom'
+import POPOSDetails from './POPOSDetails/POPOSDetails'
+
+function App() {
+  return (
+    <Router>
+
+      <div className="App">
+        <Title />
+
+        <Route exact path="/" component={POPOSList}/>
+        <Route path="/about" component={About} />
+        <Route path="/details/:id" component={POPOSDetails} />
+        <Footer />
+
+      </div>
+
+    </Router>
+  );
+}
+
+export default App;
